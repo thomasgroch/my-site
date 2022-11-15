2f706048c0d7242fb0f1574854c551f068eb4731
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-11-15 14:15:41 -0300

    update tests

--- cypress/e2e/contact-form.cy.js
+++ cypress/e2e/contact-form.cy.js
@@ -12,7 +12,7 @@ describe('formulario de contato', () => {
   //   cy.get(':nth-child(6) > .flex > .rounded').click();
   // })
 
-  it('it handle errors', () => {
+  it('it has a submit button', () => {
     cy.visit('http://localhost:3000/contato')
     // cy.get('.todo-list li').should('have.length', 2)
 
@@ -26,5 +26,19 @@ describe('formulario de contato', () => {
 
     // cy.get(':nth-child(6) > .flex > .rounded').click();
     // cy.get('div[role=alert]').should('not.be.visible')
+  })
+
+  it('and the submit button works', () => {
+    cy.visit('http://localhost:3000/contato')
+
+    cy.get('#grid-nome').type('Teste');
+    cy.get('#grid-email').type('teste@teste.com');
+    cy.get('#grid-telefone').type('12123456789');
+    cy.get('#grid-mensagem').type('Esta é uma mensagem de teste com muitas letras. Esta é uma mensagem de teste com muitas letras.');
+    cy.get('#grid-estado').select('AC');
+    cy.get('#grid-cidade').select('Assis Brasil');
+    cy.get(':nth-child(6) > .flex > .rounded').click();
+
+    cy.get('div[role=alert]').should('not.be.visible')    
   })
 })
