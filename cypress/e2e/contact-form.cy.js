b46f17ffbc92b4ba45bcf272070b9a3c647ede81
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-11-25 17:51:43 -0300

    tests passes

--- cypress/e2e/contact-form.cy.js
+++ cypress/e2e/contact-form.cy.js
@@ -28,16 +28,16 @@ describe('formulario de contato', () => {
     // cy.get('div[role=alert]').should('not.be.visible')
   })
 
-  // it('and the submit button works', () => {
-  //   cy.visit('http://localhost:3000/contato')
+  it('and the submit button works', () => {
+    cy.visit('http://localhost:3000/contato')
 
-  //   cy.get('#grid-nome').type('Teste');
-  //   cy.get('#grid-email').type('teste@teste.com');
-  //   cy.get('#grid-telefone').type('12123456789');
-  //   cy.get('#grid-mensagem').type('Esta é uma mensagem de teste com muitas letras. Esta é uma mensagem de teste com muitas letras.');
-  //   cy.get('#grid-estado').select('AC');
-  //   cy.get('#grid-cidade').select('Assis Brasil');
-  //   cy.get(':nth-child(6) > .flex > .rounded').click();
-  //   cy.contains('enviada com sucesso').should('be.visible')
-  // })
+    cy.get('#grid-nome').type('Teste');
+    cy.get('#grid-email').type('teste@teste.com');
+    cy.get('#grid-telefone').type('12123456789');
+    cy.get('#grid-mensagem').type('Esta é uma mensagem de teste com muitas letras. Esta é uma mensagem de teste com muitas letras.');
+    cy.get('#grid-estado').select('AC');
+    cy.get('#grid-cidade').select('Assis Brasil');
+    cy.get(':nth-child(6) > .flex > .rounded').click();
+    cy.contains('enviada com sucesso').should('be.visible')
+  })
 })
