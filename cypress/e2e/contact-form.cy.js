d03967894f0a427537a905654c968639f7b0c12f
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-12-05 17:39:15 -0300

    mailgun lib updated

--- cypress/e2e/contact-form.cy.js
+++ cypress/e2e/contact-form.cy.js
@@ -1,35 +1,15 @@
 describe('formulario de contato', function () {
+
   // use function instead of arrow syntax to access aliased values on `this`
-  // it('it fills and submit the form', function () {
-  //   cy.visit('/contato')
-  //   // cy.get('.todo-list li').should('have.length', 2)
-
-  //   cy.get('#grid-nome').type('Teste');
-  //   cy.get('#grid-email').type('teste@teste.com');
-  //   cy.get('#grid-telefone').type('12123456789');
-  //   cy.get('#grid-mensagem').type('Esta é uma mensagem de teste com muitas letras. Esta é uma mensagem de teste com muitas letras.');
-  //   cy.get('#grid-estado').select('AC');
-  //   cy.get('#grid-cidade').select('Assis Brasil');
-  //   cy.get(':nth-child(6) > .flex > .rounded').click();
-  // })
-
   it('it has a submit button', function () {
     cy.visit('/contato')
-    // cy.get('.todo-list li').should('have.length', 2)
-
-    cy.get('#grid-nome').type('Teste');
-    cy.get('#grid-email').type('teste@teste.com');
-    cy.get('#grid-telefone').type('12123456789');
-    cy.get('#grid-mensagem').type('Esta é uma mensagem de teste com muitas letras. Esta é uma mensagem de teste com muitas letras.');
-    cy.get('#grid-estado').select('AC');
-    cy.get('#grid-cidade').select('Assis Brasil');
     cy.get(':nth-child(6) > .flex > .rounded').should('be.visible');
 
     // cy.get(':nth-child(6) > .flex > .rounded').click();
     // cy.get('div[role=alert]').should('not.be.visible')
   })
-  
-  it('and the submit button works', function () {
+
+  it('fills and submit the form and the form submit works', function () {
     cy.visit('/contato')
 
     cy.get('#grid-nome').type('Teste');
--- functions/contact.js
+++ functions/contact.js
@@ -2,30 +2,49 @@ import {parse} from 'querystring'
 
 const faunadb = require('faunadb')
 const moment = require('moment')
-const Mailgun = require('mailgun-js')
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
 const q = faunadb.query
 const client = new faunadb.Client({
 	secret: process.env.FAUNADB_SERVER_SECRET
 })
 const {MAILGUN_API_KEY: apiKey, MAILGUN_DOMAIN: domain} = process.env
-const mailgun = Mailgun({
-	apiKey,
-	domain,
-	retry: 3
-})
+// const mailgun = Mailgun({
+// 	apiKey,
+// 	domain,
+// 	retry: 3
+// })
 
 const headers = {
 	'Access-Control-Allow-Origin': '*', // better change this for production
 	'Access-Control-Allow-Methods': 'POST',
 	'Access-Control-Allow-Headers': 'Content-Type'
 }
+//
+// const sendEmail = data => {
+// 	const {from, to, subject, text} = data
+// 	const email = {from, to, subject, text}
+//
+// 	return mailgun.messages().send(email)
+// }
 
-const sendEmail = data => {
-	const {from, to, subject, text} = data
-	const email = {from, to, subject, text}
 
-	return mailgun.messages().send(email)
-}
+// const sendEmail = async data => {
+//   const {from, to, subject, text} = data
+//   const email = {from, to, subject, text}
+//
+//   const mailgun = new Mailgun(data);
+//   const mg = mailgun.client({
+//     username: 'api',
+//     key: process.env.MAILGUN_API_KEY,
+//     public_key: process.env.MAILGUN_API_PUBLIC_KEY,
+//   });
+//   const msg = await mg.messages.send(domain, data)
+//   console.log('msg', msg);
+//   return msg
+//   // return mailgun.messages().send(email)
+// }
 
 exports.handler = async (event, context) => {
 	// only allow POST requests
@@ -75,23 +94,38 @@ exports.handler = async (event, context) => {
 			}
 		}))
 
+    const {
+      MAILGUN_API_KEY: key,
+      MAILGUN_DOMAIN: domain,
+      MAILGUN_API_PUBLIC_KEY: public_key
+    } = process.env;
+
 		const payloadMail = {
 			from: process.env.MAILGUN_FROM || 'Thomas Groch <contato@thomasgroch.com>',
 			to: payload.email,
 			subject: 'Obrigado pelo seu interesse ' + payload.nome + '.',
 			text: 'Retorno para você o mais cedo possível!'
 		}
-		const result = await sendEmail(payloadMail)
-		if (!result || !result.message) {
-			throw new Error( (result.message) ? result.message : result )
-		}
+    const mg = mailgun.client({
+      username: 'api',
+      key,
+      public_key
+    });
+    const msg = await mg.messages.create(domain, payloadMail)
+    console.log('msg', msg);
+
+		// const result = await sendEmail(payloadMail)
+		// if (!result || !result.message) {
+		// 	throw new Error( (result.message) ? result.message : result )
+		// }
 
 	} catch (error) {
+    console.log(error)
 		return {
 			statusCode: 500,
 			headers,
 			body: JSON.stringify({
-				error,
+				error
 			}),
 		}
 	}
