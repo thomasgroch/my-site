66ff37d0052230f788c4a0272e5e0e7c95bce376
Thomas Letsch Groch <thomas.groch@gmail.com>
2023-01-02 17:26:37 -0300

    meta data updated

--- functions/teste.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function (event, context) {
-  const value = process.env.LAMBDA_TASK_ROOT;
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Value of LAMBDA_TASK_ROOT is ${value}.` }),
-  };
-};
