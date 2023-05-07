214d01b94b6c791854f52280c32438c930ba6257
Thomas Letsch Groch <thomas.groch@gmail.com>
2023-05-07 20:30:57 -0300

    update

--- src/views/MeetPage.vue
+++ src/views/MeetPage.vue
@@ -5,11 +5,10 @@
     <div v-if="props.nome" class="w-full px-5 pb-5">
       <div class="flex flex-col items-center justify-center">
         <Logo class="!h-20" />
-        <p class="text-base font-bold pt-5">{{ $t('meet.' + meetType) }} Confirmed</p>
-        <!-- <p class="text-base pb-3 pt-2">You are scheduled with Thomas Groch.</p> -->
+        <p class="text-base font-bold pt-5">{{ $t('meet.' + meetType) }} {{ $t('meet.confirmed') }}</p>
+        <p class="text-base pb-3 pt-2">{{ $t('meet.you-are-scheduled-with') }} Thomas Groch.</p>
       </div>
 
-      <!-- <hr class="py-3" /> -->
       <div class="px-2 pt-5 w-96 mx-auto">
         <div>
           <span class="flex font-bold pb-1" v-if="props.date">
@@ -26,20 +25,20 @@
 
           <span class="flex text-neutral-500 pb-1" v-if="props.date">
             <GlobeAltIcon class="h-7 w-7 mr-2 text-blue-500"/>
-            <a :href="$route.path" target="_blank">Conference link</a>
+            <a :href="$route.path" target="_blank">{{ $t('meet.conference-link') }}</a>
           </span>
           <span class="flex text-neutral-500 pb-1" v-if="props.date">
             <VideoCameraIcon class="h-7 w-7 mr-2 text-blue-500"/>
-            Web conferencing details to follow.
+            {{ $t('meet.web-conferencing-details-to-follow') }}
           </span>
         </div>
 
         <div class="py-3">
           <!-- <p>A confirmation has been sent to your email address.</p> -->
 
-          <div v-if="props.date && ! itIsTime" class="w-full">
+          <span v-if="props.date && ! itIsTime" class="w-full">
             Faltam <span v-if="0 !== days">{{ days }} dias, </span>{{ hours }} horas, {{ minutes }} minutos e {{ seconds }} segundos.
-          </div>
+          </span>
         </div>
       </div>
 
@@ -90,53 +89,53 @@
   const meetTime = computed(() => props.date && props.date.split('-').length >= 5 ? `${props.date.split('-')[3]}:${props.date.split('-')[4]}` : '')
   
   const eventTime = ref(parse(`${meetDate.value} ${meetTime.value}`, 'dd/MM/yyyy HH:mm', new Date()))
-  const icsStartDateString = ref(formatISO(new Date(eventTime.value), { representation: "complete" }))
-  const oneHourLaterString = ref(formatISO(addHours(new Date(eventTime.value), 1), { representation: "complete" }))
+  // const icsStartDateString = ref(formatISO(new Date(eventTime.value), { representation: "complete" }))
+  // const oneHourLaterString = ref(formatISO(addHours(new Date(eventTime.value), 1), { representation: "complete" }))
 
-  const event = ref({
-    title: nomeCapitalized,
-    start: icsStartDateString.value,
-    end: oneHourLaterString.value,
-    location: 'Localasd do Evento',
-    description: 'Descriçãoasd do Evento',
-  })
+  // const event = ref({
+  //   title: nomeCapitalized,
+  //   start: icsStartDateString.value,
+  //   end: oneHourLaterString.value,
+  //   location: 'Localasd do Evento',
+  //   description: 'Descriçãoasd do Evento',
+  // })
 
-  const googleCalendarLink = computed(() => {
+  // const googleCalendarLink = computed(() => {
 
-    const icsStartDateString = formatISO(new Date(eventTime.value), { representation: "complete" });
-    const oneHourLaterString = formatISO(addHours(new Date(eventTime.value), 1), { representation: "complete" });
+  //   const icsStartDateString = formatISO(new Date(eventTime.value), { representation: "complete" });
+  //   const oneHourLaterString = formatISO(addHours(new Date(eventTime.value), 1), { representation: "complete" });
 
-    const action = `TEMPLATE` // TEMPLATE (required)
-    const text = `${event.value.title}` // Title of the event (URL encoded format).
-    const details = `${event.value.description}` // Event details or description (URL encoded format).
-    const dates = `${encodeURIComponent(icsStartDateString)}/${encodeURIComponent(oneHourLaterString)}` // ISO date format (start_datetime/end_datetime)
-    const location = `${event.value.location}` // Location of the event (URL encoded format).
+  //   const action = `TEMPLATE` // TEMPLATE (required)
+  //   const text = `${event.value.title}` // Title of the event (URL encoded format).
+  //   const details = `${event.value.description}` // Event details or description (URL encoded format).
+  //   const dates = `${encodeURIComponent(icsStartDateString)}/${encodeURIComponent(oneHourLaterString)}` // ISO date format (start_datetime/end_datetime)
+  //   const location = `${event.value.location}` // Location of the event (URL encoded format).
 
-    return `https://calendar.google.com/calendar/render?action=${action}&text=${text}&details=${details}&dates=${dates}&location=${location}`
-    // return https://calendar.google.com/calendar/render?action=TEMPLATE&text=My Event&details=Event description text&dates=20220305T103000/20220305T184500&location=New York City
-    // return link
-  })
+  //   return `https://calendar.google.com/calendar/render?action=${action}&text=${text}&details=${details}&dates=${dates}&location=${location}`
+  //   // return https://calendar.google.com/calendar/render?action=TEMPLATE&text=My Event&details=Event description text&dates=20220305T103000/20220305T184500&location=New York City
+  //   // return link
+  // })
 
-  const icsLink = computed(() => {
+//   const icsLink = computed(() => {
 
-    const icsStartDateString = formatISO(new Date(eventTime.value), { representation: "complete" });
-    const oneHourLaterString = formatISO(addHours(new Date(eventTime.value), 1), { representation: "complete" });
-    const calendarData = `BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-SUMMARY:${event.value.title}
-DTSTART:${icsStartDateString}
-DTEND:${oneHourLaterString}
-LOCATION:${event.value.location}
-DESCRIPTION:${event.value.description}
-END:VEVENT
-END:VCALENDAR`;
-console.log(calendarData)
+//     const icsStartDateString = formatISO(new Date(eventTime.value), { representation: "complete" });
+//     const oneHourLaterString = formatISO(addHours(new Date(eventTime.value), 1), { representation: "complete" });
+//     const calendarData = `BEGIN:VCALENDAR
+// VERSION:2.0
+// BEGIN:VEVENT
+// SUMMARY:${event.value.title}
+// DTSTART:${icsStartDateString}
+// DTEND:${oneHourLaterString}
+// LOCATION:${event.value.location}
+// DESCRIPTION:${event.value.description}
+// END:VEVENT
+// END:VCALENDAR`;
+// console.log(calendarData)
 
-    const encodedData = encodeURIComponent(calendarData);
-    const link = `data:text/calendar;charset=utf-8,${encodedData}`;
-    return link
-  })
+//     const encodedData = encodeURIComponent(calendarData);
+//     const link = `data:text/calendar;charset=utf-8,${encodedData}`;
+//     return link
+//   })
   
   const days = ref(0);
   const hours = ref(0);
@@ -158,6 +157,9 @@ console.log(calendarData)
 
   onMounted(() => {
     if ( !props.date ) {
+      return ''
+    }
+    if ( ! props.date ){
       return ''
     }
     const futureDate = new Date(eventTime.value);
